45c9ecc3cdd8d489b8106252ce4ac43f6c608a96
DylanTYX <dylantanyx@gmail.com>
2026-06-03 15:13:53 +0800

    feat: add Toaster component with customizable toast options

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Toaster as Sonner, type ToasterProps } from "sonner";
+
+export function Toaster({ ...props }: ToasterProps) {
+  return (
+    <Sonner
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-900 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-soft-md",
+          description: "group-[.toast]:text-gray-500",
+          actionButton:
+            "group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50",
+          cancelButton:
+            "group-[.toast]:bg-gray-100 group-[.toast]:text-gray-600",
+        },
+      }}
+      {...props}
+    />
+  );
+}
